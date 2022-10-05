a6c2e52161a102b27e9dd720b8ece22653784994
grinn <grinnosse@gmail.com>
2022-10-05 16:16:04 +0300

    Initial

---
+++ core/src/main/java/liga/common/commonmodule/core/CommonModuleApplication.java
@@ -0,0 +1,15 @@
+package liga.common.commonmodule.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"liga.common.commonmodule", "liga.common.commonmodule.service"})
+public class CommonModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonModuleApplication.class, args);
+    }
+
+}
